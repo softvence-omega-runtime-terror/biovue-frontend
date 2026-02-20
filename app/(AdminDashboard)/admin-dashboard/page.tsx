9c64350fd3584e52b042231aaef02888bf34b530
Joyeta-Mondal <dipannitakotha2019@gmail.com>
2026-02-20 16:06:25 +0600

    created admin and trainer dashboard

---
+++ app/(AdminDashboard)/admin-dashboard/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+const AdminDashboard = () => {
+  return (
+    <div className="p-8">
+      <h1 className="text-3xl font-bold mb-6">Admin Dashboard</h1>
+    </div>
+  );
+}
+export default AdminDashboard;
---
+++ app/(TrainerDashboard)/trainer-dashboard/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+const TrainerDashboard = () => {
+  return (
+    <div className="p-8">
+      <h1 className="text-3xl font-bold mb-6">Trainer Dashboard</h1>
+    </div>
+  );
+}
+export default TrainerDashboard;
