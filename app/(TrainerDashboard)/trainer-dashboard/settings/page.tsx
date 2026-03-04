cc8cd0ea70e76b378e065c9091402cadc8e71b0c
Joyeta-Mondal <dipannitakotha2019@gmail.com>
2026-03-04 09:45:32 +0600

    initiated settings page

---
+++ app/(TrainerDashboard)/trainer-dashboard/settings/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import DashboardHeading from "@/components/common/DashboardHeading";
+
+export default function SettingsPage() {
+  return (
+    <div>
+      {/* heading */}
+      <div>
+        <DashboardHeading
+          heading="Settings"
+          subheading="Manage your account preferences, notifications, and coaching controls"
+        />
+      </div>
+    </div>
+  );
+}
