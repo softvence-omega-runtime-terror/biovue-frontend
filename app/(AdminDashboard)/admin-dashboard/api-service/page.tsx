ef9ea3d6a7caf9bc736cfb80a931e2cfad9c4a92
Joyeta-Mondal <dipannitakotha2019@gmail.com>
2026-03-12 08:50:42 +0600

    created api service page

---
+++ app/(AdminDashboard)/admin-dashboard/api-service/page.tsx
@@ -0,0 +1,9 @@
+import DashboardHeading from "@/components/common/DashboardHeading";
+
+export default function apiservicePage() {
+  return (
+    <div>
+      <DashboardHeading heading="API Service" />
+    </div>
+  );
+}
