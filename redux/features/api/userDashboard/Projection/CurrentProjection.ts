9fdb6f47138da510319ad7897438a90808d8b8cf
Joyeta-Mondal <dipannitakotha2019@gmail.com>
2026-03-16 13:01:46 +0600

    changed loader in projection

--- redux/features/api/userDashboard/Projection/CurrentProjection.ts
+++ redux/features/api/userDashboard/Projection/CurrentProjection.ts
@@ -43,7 +43,6 @@ export const projectionApiEndpoints = projectionApi.injectEndpoints({
           body: formData,
         };
       },
-      invalidatesTags: ["Projection"],
     }),
   }),
   overrideExisting: true,
